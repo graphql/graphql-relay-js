d6c5466723ae60274f5362ce64db0e2ce1250507
Lee Byron <lee@leebyron.com>
2016-11-15 21:06:54 -0800

    Don't build and test on npm install
    Speed up travis builds, speed up local installs, unbreak 0.12 tests

---
+++ resources/prepublish.sh
@@ -0,0 +1,8 @@
+# Because of a long-running npm issue (https://github.com/npm/npm/issues/3059)
+# prepublish runs after `npm install` and `npm pack`.
+# In order to only run prepublish before `npm publish`, we have to check argv.
+if node -e "process.exit(($npm_config_argv).original[0].indexOf('pu') === 0)"; then
+  exit 0;
+fi
+
+npm test && npm run build
