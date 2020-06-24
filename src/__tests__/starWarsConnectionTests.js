e295257fca94299f3c26736fa4d1bc3c02502be6
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-06-24 16:03:39 +0300

    Flow: Switch all files to '@flow strict'

--- src/__tests__/starWarsConnectionTests.js
+++ src/__tests__/starWarsConnectionTests.js
@@ -1,4 +1,4 @@
-// @flow
+// @flow strict
 
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
--- src/connection/__tests__/arrayconnection.js
+++ src/connection/__tests__/arrayconnection.js
@@ -1,4 +1,4 @@
-// @flow
+// @flow strict
 
 import { describe, it } from 'mocha';
 import { expect } from 'chai';
