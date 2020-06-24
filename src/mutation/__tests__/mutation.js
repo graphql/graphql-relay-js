00e31aa44524e68fd647f74d335e3ab7179a353c
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-06-24 15:37:44 +0300

    Update LICENSE and remove license header from source files
    Replicates https://github.com/graphql/graphql-js/pull/1960

--- src/mutation/__tests__/mutation.js
+++ src/mutation/__tests__/mutation.js
@@ -1,11 +1,4 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- *
- * @flow
- */
+// @flow
 
 // 80+ char lines are useful in describe/it, so ignore in this file.
 /* eslint-disable max-len */
