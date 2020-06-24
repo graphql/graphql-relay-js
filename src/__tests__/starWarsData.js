cee194eaeb6825f67bd408e562edf3d6cad36719
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2020-06-24 15:58:28 +0300

    Update LICENSE and remove license header from source files (#250)

--- src/__tests__/starWarsData.js
+++ src/__tests__/starWarsData.js
@@ -1,9 +1,4 @@
-/**
- * Copyright (c) 2015-present, Facebook, Inc.
- *
- * This source code is licensed under the MIT license found in the
- * LICENSE file in the root directory of this source tree.
- */
+// @flow
 
 /**
  * This defines a basic set of data for our Star Wars Schema.
