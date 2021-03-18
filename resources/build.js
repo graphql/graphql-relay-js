87e62e7a5436926211b220d9e9f43e892aa82a1c
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2021-03-18 14:38:48 +0200

    Sync package building process with 'graphql-js' (#316)

--- resources/build.js
+++ resources/build.js
@@ -1,5 +1,3 @@
-// @noflow
-
 'use strict';
 
 const fs = require('fs');
@@ -8,40 +6,49 @@ const assert = require('assert');
 
 const babel = require('@babel/core');
 
-const { rmdirRecursive, readdirRecursive, showStats } = require('./utils');
+const { readdirRecursive, showDirStats } = require('./utils');
 
 if (require.main === module) {
-  rmdirRecursive('./dist');
-  fs.mkdirSync('./dist');
+  fs.rmSync('./npmDist', { recursive: true, force: true });
+  fs.mkdirSync('./npmDist');
 
   const srcFiles = readdirRecursive('./src', { ignoreDir: /^__.*__$/ });
   for (const filepath of srcFiles) {
     const srcPath = path.join('./src', filepath);
-    const destPath = path.join('./dist', filepath);
+    const destPath = path.join('./npmDist', filepath);
 
     fs.mkdirSync(path.dirname(destPath), { recursive: true });
     if (filepath.endsWith('.js')) {
-      fs.copyFileSync(srcPath, destPath + '.flow');
+      const flowBody = '// @flow strict\n' + fs.readFileSync(srcPath, 'utf-8');
+      fs.writeFileSync(destPath + '.flow', flowBody);
 
       const cjs = babelBuild(srcPath, { envName: 'cjs' });
       fs.writeFileSync(destPath, cjs);
-    } else if (filepath.endsWith('d.ts')) {
+    } else if (filepath.endsWith('.d.ts')) {
       fs.copyFileSync(srcPath, destPath);
     }
   }
 
-  fs.copyFileSync('./LICENSE', './dist/LICENSE');
-  fs.copyFileSync('./README.md', './dist/README.md');
+  fs.copyFileSync('./LICENSE', './npmDist/LICENSE');
+  fs.copyFileSync('./README.md', './npmDist/README.md');
 
   // Should be done as the last step so only valid packages can be published
   const packageJSON = buildPackageJSON();
-  fs.writeFileSync('./dist/package.json', JSON.stringify(packageJSON, null, 2));
+  fs.writeFileSync(
+    './npmDist/package.json',
+    JSON.stringify(packageJSON, null, 2),
+  );
 
-  showStats();
+  showDirStats('./npmDist');
 }
 
 function babelBuild(srcPath, options) {
-  return babel.transformFileSync(srcPath, options).code + '\n';
+  const { code } = babel.transformFileSync(srcPath, {
+    babelrc: false,
+    configFile: './.babelrc-npm.json',
+    ...options,
+  });
+  return code + '\n';
 }
 
 function buildPackageJSON() {
@@ -54,16 +61,19 @@ function buildPackageJSON() {
   delete packageJSON.devDependencies;
 
   const { version } = packageJSON;
-  const versionMatch = /^\d+\.\d+\.\d+-?(.*)?$/.exec(version);
+  const versionMatch = /^\d+\.\d+\.\d+-?(?<preReleaseTag>.*)?$/.exec(version);
   if (!versionMatch) {
     throw new Error('Version does not match semver spec: ' + version);
   }
 
-  const [, preReleaseTag] = versionMatch;
+  const { preReleaseTag } = versionMatch.groups;
 
   if (preReleaseTag != null) {
     const [tag] = preReleaseTag.split('.');
-    assert(['alpha', 'beta', 'rc'].includes(tag), `"${tag}" tag is supported.`);
+    assert(
+      tag.startsWith('experimental-') || ['alpha', 'beta', 'rc'].includes(tag),
+      `"${tag}" tag is supported.`,
+    );
 
     assert(!packageJSON.publishConfig, 'Can not override "publishConfig".');
     packageJSON.publishConfig = { tag: tag || 'latest' };
