fc62adeef64947b4c1985376547f537a3ec8fa1b
Ivan Goncharov <ivan.goncharov.ua@gmail.com>
2021-03-20 09:41:01 +0200

    connectiontypes: inline types into 'connection.js' (#320)

--- src/connection/connection.d.ts
+++ src/connection/connection.d.ts
@@ -41,6 +41,21 @@ export const connectionArgs: GraphQLFieldConfigArgumentMap &
   ForwardConnectionArgs &
   BackwardConnectionArgs;
 
+/**
+ * A type alias for cursors in this implementation.
+ */
+export type ConnectionCursor = string;
+
+/**
+ * A type describing the arguments a connection field receives in GraphQL.
+ */
+export interface ConnectionArguments {
+  before?: ConnectionCursor | null;
+  after?: ConnectionCursor | null;
+  first?: number | null;
+  last?: number | null;
+}
+
 export interface ConnectionConfig {
   name?: string;
   nodeType: GraphQLObjectType;
@@ -62,3 +77,29 @@ export interface GraphQLConnectionDefinitions {
 export function connectionDefinitions(
   config: ConnectionConfig,
 ): GraphQLConnectionDefinitions;
+
+/**
+ * A type designed to be exposed as a `Connection` over GraphQL.
+ */
+export interface Connection<T> {
+  edges: Array<Edge<T>>;
+  pageInfo: PageInfo;
+}
+
+/**
+ * A type designed to be exposed as a `Edge` over GraphQL.
+ */
+export interface Edge<T> {
+  node: T;
+  cursor: ConnectionCursor;
+}
+
+/**
+ * A type designed to be exposed as `PageInfo` over GraphQL.
+ */
+export interface PageInfo {
+  startCursor: ConnectionCursor | null;
+  endCursor: ConnectionCursor | null;
+  hasPreviousPage: boolean;
+  hasNextPage: boolean;
+}
